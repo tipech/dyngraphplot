aeebf7bb05e325852204d3788e0d4af6f4f3b099
Tilemachos Pechlivanoglou <tppehli@gmail.com>
2018-11-06 02:33:22 -0500

    Version fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dyngraphplot",
-    version="1.1.2",
+    version="1.1.3",
     author="Tilemachos Pechlivanoglou",
     author_email="tppehli@gmail.com",
     description="A module for plotting dynamic force-directed graphs",
